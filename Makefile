cf056ca2db8b2ebf4fa46aabed81a73a663b9c4b
Allan Wilson <akwilson76@googlemail.com>
2020-12-13 17:35:24 +0000

    Add support for emscripten to Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
+SRCS = graphics.c tetris.c tetronimoes.c
+
 BIN1 = tetris
-BIN1_SRCS = graphics.c tetris.c tetronimoes.c
+BIN1_SRCS = $(SRCS)
 LIBS = -lSDL2 -lSDL2_image -lSDL2_ttf
+
+WASM1 = tetris.js
+WASM1_SRCS = $(SRCS)
+WASM1_ASSETS = assets
+WASM1_USE_SDL2 = Y
 
 include lib/simplified.mk
--- lib/simplified.mk
+++ lib/simplified.mk
@@ -26,13 +26,27 @@ TST1_OUT = $(addprefix $(BUILDDIR)/, $(TST1))
 TST1_OBJS = $(TST1_SRCS:%.c=$(addprefix $(BUILDDIR)/, %.o))
 TST1_DEPS = $(TST1_OBJS:%.o=%.d)
 
+WASM1_OUT = $(addprefix $(BUILDDIR)/, $(WASM1))
+WASM_CORE = $(basename $(WASM1))
+WASM1_JS = $(addprefix $(BUILDDIR)/, $(WASM_CORE).js)
+WASM1_WASM = $(addprefix $(BUILDDIR)/, $(WASM_CORE).wasm)
+WASM1_DATA = $(addprefix $(BUILDDIR)/, $(WASM_CORE).data)
+WASM1_HTML = $(addprefix $(BUILDDIR)/, $(WASM_CORE).html)
+WASM_CC = emcc
+ifeq ($(WASM1_USE_SDL2),Y)
+	WASM_EXTRAS = -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2
+endif
+ifneq ($(WASM1_ASSETS),)
+	WASM1_ASSETS_ARGS = --preload-file $(WASM1_ASSETS)
+endif
+
 CFLAGS = -g -I. $(INCLUDE_PATH) -Wall -Wextra
 #LDFLAGS = -linker_flags
 
 $(BUILDDIR)/%.o : %.c
 	$(COMPILE.c) -MMD -fpic -o $@ $<
 
-all : directories $(LIB1_OUT) $(BIN1_OUT) $(TST1_OUT)
+all : directories $(LIB1_OUT) $(BIN1_OUT) $(TST1_OUT) $(WASM1_OUT)
 
 $(LIB1_OUT) : $(LIB1_OBJS)
 	$(LINK.c) $^ -shared -o $@
@@ -42,6 +56,9 @@ $(BIN1_OUT) : $(BIN1_OBJS)
 
 $(TST1_OUT) : $(TST1_OBJS)
 	$(LINK.c) $^ $(LIB_PATH) $(LIBS) -o $@
+
+$(WASM1_OUT) : $(WASM1_SRCS)
+	$(WASM_CC) $^ $(WASM_EXTRAS) $(WASM1_ASSETS_ARGS) -o $@
 
 -include $(LIB1_DEPS) $(BIN1_DEPS) $(TST1_DEPS)
 
@@ -56,7 +73,8 @@ directories :
 clean :
 	rm -f $(LIB1_OBJS) $(LIB1_DEPS) $(LIB1_OUT) \
 		  $(BIN1_OBJS) $(BIN1_DEPS) $(BIN1_OUT) \
-		  $(TST1_OBJS) $(TST1_DEPS) $(TST1_OUT)
+		  $(TST1_OBJS) $(TST1_DEPS) $(TST1_OUT) \
+		  $(WASM1_HTML) $(WASM1_JS) $(WASM1_DATA) $(WASM1_WASM)
 
 tests :
 	@LD_LIBRARY_PATH=$(LIB_PATH) DYLD_LIBRARY_PATH=$(LIB_PATH) $(TST1_OUT)
